f8ee107f70604014f342f217ebc462cbd7f01788
zakarya hari <zakariahari42@gmail.com>
2026-02-24 05:30:09 +0000

    feat: création de la table invitations avec email, token, status et colocation_id

---
+++ database/migrations/2026_02_24_043255_create_invitations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('token')->unique();
+            $table->enum('status', ['pending', 'accepted', 'refused'])->default('pending');
+            $table->foreignId('colocation_id')->constrained('colocations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
