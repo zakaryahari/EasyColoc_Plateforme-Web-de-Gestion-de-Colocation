9b963a69ddeee9f51b573c183d30a1d708c2833b
zakarya hari <zakariahari42@gmail.com>
2026-02-24 05:30:29 +0000

    feat: création de la table expense_shares avec expense_id, user_id, amount et is_paid

---
+++ database/migrations/2026_02_24_043256_create_expense_shares_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense_shares', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('expense_id')->constrained('expenses')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users');
+            $table->decimal('amount', 10, 2);
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense_shares');
+    }
+};
