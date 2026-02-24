4cf2af99741c24d188a12b5df1bb369b5dfd8e4f
zakarya hari <zakariahari42@gmail.com>
2026-02-24 05:30:24 +0000

    feat: création de la table expenses avec title, amount, date, payer_id, colocation_id et category_id

---
+++ database/migrations/2026_02_24_043256_create_expenses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->decimal('amount', 10, 2);
+            $table->date('date');
+            $table->foreignId('payer_id')->constrained('users');
+            $table->foreignId('colocation_id')->constrained('colocations');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
