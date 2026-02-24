44badb6d7573b5503c31e6c0a653d444c8ef4e69
zakarya hari <zakariahari42@gmail.com>
2026-02-24 05:38:00 +0000

    feat: création du modèle Colocation avec relations members, owner, expenses et invitations

---
+++ app/Models/Colocation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    protected $fillable = [
+        'name',
+        'status',
+        'owner_id',
+    ];
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'colocation_user')
+            ->using(Membership::class)
+            ->withPivot('role', 'joined_at', 'left_at')
+            ->withTimestamps();
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+}
