19e949361d2e6c732fc309108db3090a84ec2bdc
zakarya hari <zakariahari42@gmail.com>
2026-02-24 05:38:08 +0000

    feat: création du modèle Category avec relations colocation et expenses

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'colocation_id',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+}
