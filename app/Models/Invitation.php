ebde6a10e120e5238fb215d3306bc3ef3e0a97d1
zakarya hari <zakariahari42@gmail.com>
2026-02-24 05:38:15 +0000

    feat: création du modèle Invitation avec relation colocation

---
+++ app/Models/Invitation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invitation extends Model
+{
+    protected $fillable = [
+        'email',
+        'token',
+        'status',
+        'colocation_id',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+}
