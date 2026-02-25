e5ef2c2cf2c791d6aadd05db9e9e61d0cf0ff74f
zakarya hari <zakariahari42@gmail.com>
2026-02-25 09:58:59 +0000

    feat: ajout la column description a la table colocation

--- database/migrations/2026_02_24_043135_create_colocations_table.php
+++ database/migrations/2026_02_24_043135_create_colocations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('colocations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description');
             $table->enum('status', ['active', 'cancelled'])->default('active');
             $table->foreignId('owner_id')->constrained('users');
             $table->timestamps();
