d2fca2cfe30b8febd44199eb0a15a34ad587eda2
zakarya hari <zakariahari42@gmail.com>
2026-02-25 00:54:30 +0000

    fix: correction de l'enregistrement du middleware CheckBanned pour éviter les erreurs fatales

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'check.banned' => \App\Http\Middleware\CheckBanned::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
