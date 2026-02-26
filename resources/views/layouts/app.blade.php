0be02bbb207a3ba0570194b01551722ef846fce8
zakarya hari <zakariahari42@gmail.com>
2026-02-26 23:47:04 +0000

    design: thème sombre pour auth et composants

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,9 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @if(!request()->routeIs('colocations.*'))
+                @include('layouts.navigation')
+            @endif
 
             <!-- Page Heading -->
             @isset($header)
